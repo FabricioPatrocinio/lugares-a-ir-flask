d8713d970c01c1b10238c9830f9c08b92bd50c59
fabriciopatrocinio <fabricioschiffer@gmail.com>
2021-03-22 16:10:57 -0300

    All very well

--- app/static/js/glider.js
+++ app/static/js/glider.js
@@ -1,10 +1,13 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: '.dots',
-    draggable: true,
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    }
-});
+var sliders = document.querySelectorAll('.glider');
+var dots = document.querySelectorAll('.dots');
+
+for (var i = 0; i < sliders.length; i++) {
+    var glide = new Glider(sliders[i], {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: dots[i],
+        draggable: true
+    });
+}
+
+glide.mount();
